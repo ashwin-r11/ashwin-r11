247141525b88fe1a011853ca2da3511462e9cbf7
Ashwin R <144155895+ashwin-r11@users.noreply.github.com>
2025-09-18 23:19:57 +0530

    removed 0 from 05 in __main__(uptime part)

--- today.py
+++ today.py
@@ -448,7 +448,7 @@ if __name__ == '__main__':
     user_data, user_time = perf_counter(user_getter, USER_NAME)
     OWNER_ID, acc_date = user_data
     formatter('account data', user_time)
-    age_data, age_time = perf_counter(daily_readme, datetime.datetime(2023, 9, 05))
+    age_data, age_time = perf_counter(daily_readme, datetime.datetime(2023, 9, 5))
     formatter('age calculation', age_time)
     total_loc, loc_time = perf_counter(loc_query, ['OWNER', 'COLLABORATOR', 'ORGANIZATION_MEMBER'], 7)
     formatter('LOC (cached)', loc_time) if total_loc[-1] else formatter('LOC (no cache)', loc_time)
@@ -478,5 +478,6 @@ if __name__ == '__main__':
 
     print('Total GitHub GraphQL API calls:', '{:>3}'.format(sum(QUERY_COUNT.values())))
     for funct_name, count in QUERY_COUNT.items(): print('{:<28}'.format('   ' + funct_name + ':'), '{:>6}'.format(count))
+
 
 
